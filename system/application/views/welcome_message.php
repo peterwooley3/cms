3766331be6e648aaa39516ed7ebeedc9ebb7c3f3
Peter Wooley <peterwooley+3@gmail.com>
2010-07-19 19:46:45 -0700

    Modified Welcome Message.

--- system/application/views/welcome_message.php
+++ system/application/views/welcome_message.php
@@ -5,7 +5,7 @@
 <style type="text/css">
 
 body {
- background-color: #fff;
+ background-color: #123456;
  margin: 40px;
  font-family: Lucida Grande, Verdana, Sans-serif;
  font-size: 14px;
@@ -44,18 +44,6 @@ code {
 <body>
 
 <h1>Welcome to CodeIgniter!</h1>
-
-<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-<p>If you would like to edit this page you'll find it located at:</p>
-<code>system/application/views/welcome_message.php</code>
-
-<p>The corresponding controller for this page is found at:</p>
-<code>system/application/controllers/welcome.php</code>
-
-<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-
-
 <p><br />Page rendered in {elapsed_time} seconds</p>
 
 </body>
